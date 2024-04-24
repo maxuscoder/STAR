918df1f82467a16a3d735587466f7e0f48ead07f
Paul Muntean <paulmunteanst@gmail.com>
2024-04-24 18:04:42 +0300

    Updated proxy server

--- ISS_Tracking/server.js
+++ ISS_Tracking/server.js
@@ -5,14 +5,24 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const url = 'http://api.open-notify.org/iss-now.json';
-  https.get(url, apiResponse => {
-    apiResponse.pipe(res);
+  const options = {
+    hostname: 'api.open-notify.org', // Domain of the Open Notify API
+    port: 80, // HTTP port (since the API doesn't have HTTPS)
+    path: '/iss-now.json',
+    method: 'GET',
+  };
+
+  const proxyRequest = https.request(options, apiResponse => {
+    apiResponse.pipe(res); // Forward the response
+  });
+
+  req.pipe(proxyRequest); // Forward the original request to the proxy
+
+  proxyRequest.on('error', e => {
+    console.error(`Problem with proxy request: ${e.message}`);
   });
 });
 
 server.listen(port, hostname, () => {
-  console.log(
-    `Server running at http://<span class="math-inline">\{hostname\}\:</span>{port}/`
-  );
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
